8a8ed864bedff9163a8671e07bfa5f0220c2ec4a
AkharawitBuz <akharawitbuzzwoo@gmail.com>
2017-11-11 16:48:27 +0700

    This is updated mongoose, connect db false, import admin template false

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
+var configDB = require('./_config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url).
+  then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
