e68e1f75c899b57e4a278401b51dd40144136da5
AkharawitBuz <akharawitbuzzwoo@gmail.com>
2017-11-12 17:29:23 +0700

    This updated front end layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface, SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+
+	public show: boolean = true;
+
+	public slides = [
+		'banner-slidder.jpg',
+		'banner-slidder.jpg',
+		'banner-slidder.jpg',
+		'banner-slidder.jpg',
+		'banner-slidder.jpg',
+		'banner-slidder.jpg'
+	];
+
+	public type: string = 'component';
+
+	public disabled: boolean = false;
+
+	public config: SwiperConfigInterface = {
+		keyboard: true,
+		autoplay: {
+			delay: 5000,
+		},
+		fadeEffect: {
+			crossFade: true
+		},
+		mousewheel: true,
+		scrollbar: false,
+		navigation: true,
+		pagination: true
+	};
+
+	public onIndexChange(index: number) {
+		console.log('Swiper index: ', index);
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+	direction: 'horizontal',
+	slidesPerView: 'auto',
+	keyboard: true
+};
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		SwiperModule
+	],
+	providers: [{
+		provide: SWIPER_CONFIG,
+		useValue: DEFAULT_SWIPER_CONFIG
+	}],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
